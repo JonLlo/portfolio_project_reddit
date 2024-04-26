96994149f41de28001d7250f781c8da87eebc8ba
Jonny Lloyd <154521758+JonLlo@users.noreply.github.com>
2024-04-26 12:04:44 +0100

    lt

--- src/features/Header/Header.js
+++ src/features/Header/Header.js
@@ -1,17 +1,34 @@
-import React from 'react';
+
+import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
 
 
 
 export const Header = () => {
- 
+
+  const dispatch = useDispatch();
+  const [searchValue, setSearchValue] = useState('');
+
+
+  const handleSubredditChange = (searchValue ) => {
+    // Do whatever you need with the subreddit value
+    console.log('Selected subreddit:', searchValue);
+  };
+
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
 
   return (
-    <form onSubmit={console.log("blye")}>
-    <input
-      type="text"
-      placeholder="Search posts..."
-   
-    />
+    <form onSubmit={(event) => handleSubredditChange(searchValue)}>
+<input
+  type="text"
+  placeholder="Search posts..."
+  value={searchValue}
+  onChange={handleChange} //letters show in box when you type them
+  
+/>
     <button type="submit">Search</button>
   </form>
   );
